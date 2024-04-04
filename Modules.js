97542dd09807f7ff04b3e8dc89ad20d576a7d0ea
RAGHAW PATHAK <raghawpathak001@gmail.com>
2024-04-04 15:39:49 +0530

    first commit

---
+++ Modules.js
@@ -0,0 +1,10 @@
+// const SignUP = document.getElementById('up')
+// const SignIN = document.getElementById('in')
+
+// SignUP.addEventListener('click', ()=>{
+//     window.location.href = './SignUp.html'
+// });
+
+// SignIN.addEventListener('click', ()=>{
+//     window.location.href = './SignIn.html'
+// });
